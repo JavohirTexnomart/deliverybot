4551a973b310d4cf8ff3d943bc73aebcecb8859d
JavohirTexnomart <84893052+JavohirTexnomart@users.noreply.github.com>
2021-05-27 11:46:11 +0500

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+sslify = SSLify(app)
+
+@app.route('/', methods=["POST"])
+def hello():
+    return 'ok'
+
+@app.route('/', methods=["GET"])
+def getHelloWorld():
+    return 'ok'
+  
+if __name__ == 'main':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
