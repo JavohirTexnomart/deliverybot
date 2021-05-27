0af359fd13ddbe979383c6e14ca5a80463e72a8b
JavohirTexnomart <84893052+JavohirTexnomart@users.noreply.github.com>
2021-05-27 11:03:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,5 +21,5 @@ def hello():
 
 if __name__ == 'main':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = 5000
     app.run(host='0.0.0.0', port=port)
