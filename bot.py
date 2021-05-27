60c69515ef441320e59015c9fdd7ee6c78541b3c
JavohirTexnomart <84893052+JavohirTexnomart@users.noreply.github.com>
2021-05-27 10:15:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,85 @@
-from flask import Flask, request
+  
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This is a simple echo bot using decorators and webhook with flask
+# It echoes any incoming text messages and does not use the polling method.
+
+import logging
+import time
+
+import flask
+
 import telebot
-from flask_sslify import SSLify
-import urllib.request
-import requests
-import json
 
-app = Flask(__name__)
-sslify = SSLify(app)
+API_TOKEN = '1886558847:AAHJEcszrZFebZUxbwLTK1X67ZKCNLmYyLM'
 
-secret = "e382d9ee722840d6a53e02d299331f4f"
-bot = telebot.TeleBot('1886558847:AAHJEcszrZFebZUxbwLTK1X67ZKCNLmYyLM')
+WEBHOOK_HOST = ''
+WEBHOOK_PORT = 80  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(API_TOKEN)
+
+app = flask.Flask(__name__)
+
+
+# Empty webserver index, return nothing, just http 200
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+
+# Process webhook calls
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
+
+
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
 bot.remove_webhook()
-bot.set_webhook(url="https://javohirtexnomart.pythonanywhere.com/{}".format(secret))
 
-@app.route('/{}'.format(secret), methods=["POST"])
-def getMessage():
-    update = request.get_json()
-    if "message" in update:
-        #url = 'http://vpn.texnomart.uz:8282/javoxir_ut_trendy/hs/deliverywebhook/e382d9ee722840d6a53e02d299331f4f'
-        #myobj = str(update)
-        #jsontext = json.dumps(myobj)
-        #x = http.request('POST', url,body=jsontext)
-        #text = update["message"]["text"]
-        #chat_id = update["message"]["chat"]["id"]
-        # bot.send_message(chat_id, "you said '{}'".format(text))
-        #bot.send_message(chat_id, "{}".format(str(update)))
-        return "ok"
-    return "ok"
+time.sleep(0.1)
 
-@app.route('/', methods=["GET"])
-def getHelloWorld():
-    url = 'http://vpn.texnomart.uz:8282/javoxir_ut_trendy/hs/deliverywebhook/e382d9ee722840d6a53e02d299331f4f'
-    weburl = requests.get(url)
-    return str(weburl.text)
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Start flask server
+app.run(host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+        debug=True)
