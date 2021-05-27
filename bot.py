d944133c36a66d9c842244886187a0a31ffd7047
JavohirTexnomart <84893052+JavohirTexnomart@users.noreply.github.com>
2021-05-27 11:29:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,18 +7,23 @@ URL = 'http://vpn.texnomart.uz:8282/javoxir_ut_trendy/hs/deliverywebhook/e382d9e
 TOKEN = '1886558847:AAHJEcszrZFebZUxbwLTK1X67ZKCNLmYyLM'
 
 app = Flask(__name__)
+secret = "e382d9ee722840d6a53e02d299331f4f"
 bot = telebot.TeleBot(TOKEN)
+bot.remove_webhook()
+bot.set_webhook(url="https://https://deliverytelegrambot.herokuapp.com/{}".format(secret))
 
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/{}'.format(secret), methods=["POST"])
 def hello():
     if request.method == 'POST':
         r = request.get_json()
         myobj = json.dumps(str(r))
         weburl = requests.post(URL,data = myobj)
         return jsonify(r)
-    return '<a href=\'login/\'>/login/</a><br> <a href=\'auth/\'>/auth/</a><br><a href=\'users/\'>/users/</a><br>'
+    return 'ok'
 
+@app.route('/', methods=["GET"])
+def getHelloWorld():
+    return 'ok'
 if __name__ == 'main':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
